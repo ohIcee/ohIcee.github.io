c529019a848e0092e23ddcefa56109a55f9db218
marko <icevx1@gmail.com>
2019-10-28 22:10:20 +0100

    almost done - no picture

--- js/index_script.js
+++ js/index_script.js
@@ -7,4 +7,29 @@ function openMail() {
 
 particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
     console.log('callback - particles.js config loaded');
-  });
+});
+
+var createClickHandler = function(name) {
+    return function() { 
+
+        if (name == "StelingWebsite") {
+            window.open("http://steling.si/", "_blank");
+            return;
+        }
+
+        if (name == "stin") {
+            window.open("https://www.stin.si/", "_blank");
+            return;
+        }
+
+        window.open("/projects/"+name, "_blank");
+    };
+}
+
+var projects = document.getElementsByClassName("project");
+for(var i = 0; i < projects.length; i++)
+{
+   var projName = projects[i].id;
+
+   projects[i].onclick = createClickHandler(projName);
+}
